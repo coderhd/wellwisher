d5e67167ad3d47b83b6df6e61dd0e8c43720f8ae
CoderHD <harshdave1095@gmail.com>
2026-09-06 22:12:06 +0530

    test: verify wellwisher mvp vertical slice

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,28 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  esbuild: {
-    jsx: 'automatic',
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./src/test/setup.ts'],
-  },
+	esbuild: {
+		jsx: 'automatic',
+	},
+	test: {
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: ['./src/test/setup.ts'],
+		include: [
+			'tests/**/*.{test,spec}.{ts,tsx}',
+			'src/**/*.{test,spec}.{ts,tsx}',
+		],
+		exclude: ['**/node_modules/**', '**/e2e/**', '**/.next/**'],
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json', 'html'],
+			exclude: [
+				'**/node_modules/**',
+				'**/e2e/**',
+				'**/*.test.{ts,tsx}',
+				'**/*.spec.{ts,tsx}',
+				'src/test/**',
+			],
+		},
+	},
 })
